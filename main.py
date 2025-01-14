985d9f18aab51780173fc0cd94016af376320b50
Vladimir <volodyaklever@gmail.com>
2025-01-14 20:12:36 +0300

    module_13_4.py
    14.01.2025 Задача "Цепочка вопросов".

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
-# module_13_3.py
-# 13.01.2025 Задача "Он мне ответил!".
+# module_13_4.py
+# 14.01.2025 Задача "Цепочка вопросов".
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 import asyncio
 
 api = 'ключ'
-bot = Bot(token = api)
-dp = Dispatcher(bot, storage = MemoryStorage())
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(commands = ['start'])
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Calories")
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_handler(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    await message.answer(
+        f"Ваша норма калорий {(10 * int(data['weight'])) + (6.25 * int(data['growth'])) - (5 * int(data['age'])) + 5}")
+    await state.finish()
+
+
+@dp.message_handler(commands=['start'])
 async def start(message):
     await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
 
 @dp.message_handler()
 async def all_message(message):
     await message.answer('Введите команду /start, чтобы начать общение.')
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
